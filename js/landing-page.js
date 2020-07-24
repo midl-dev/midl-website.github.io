856716707123c996cecd261ca836c5278b57ca4f
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-07-24 09:06:06 +0200

    do not make the menu disappear

--- js/landing-page.js
+++ js/landing-page.js
@@ -14,11 +14,6 @@ $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 $('div.modal').on('show.bs.modal', function() {
 	var modal = this;
 	var hash = modal.id;
